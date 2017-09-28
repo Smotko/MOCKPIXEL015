f3539d96f7a6a6db0e7b8bc645cb3604a4a81658
Anže Pečar <anze@pecar.me>
2017-09-28 21:17:15 +0100

    Add some components

---
+++ dashboard/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tile is-parent">
+    <article class="tile is-child notification" v-bind:class="[status]">
+      <p class="subtitle">{{title}}</p>
+      <p class="title">{{content}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: ['title', 'content', 'status'],
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ dashboard/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="http://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28">
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {}
+  }
+}
+</script>
