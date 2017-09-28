ff69616a8327a873481de90631bb557438f3f0c3
Anže Pečar <anze@pecar.me>
2017-09-28 21:44:40 +0100

    Add more cards

--- dashboard/src/components/Content.vue
+++ dashboard/src/components/Content.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container is-fluid">
+
     <div class="tile is-ancestor">
       <div class="tile is-vertical is-12">
+        <div calss="tile">
+          <h1 class="title">Parental controls</h1>
+        </div>
         <div class="tile">
           <card title="Car status" content="All OK" status="is-primary"></card>
           <card title="Parental controls" content="Off" status="is-warning"></card>
           <card title="Geofence" content="Not defined"></card>
         </div>
+        <div class="tile">
+          <h1 class="title">Car overview</h1>
+        </div>
+        <div class="tile">
+          <card title="Engine" content="Off"></card>
+          <card title="Fuel level" content="90%" status="is-primary"></card>
+          <card title="Milage" content="1337"></card>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
--- dashboard/src/components/NavBar.vue
+++ dashboard/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-white" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="/">
         <img src="http://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28">
