9870d78e7bed54c9717b2934e54d6ab0f7e702c6
Anže Pečar <anze@pecar.me>
2017-10-02 22:58:58 +0100

    Refactor the backend a bit

--- backend/models_app.py
+++ backend/models_app.py
@@ -1,28 +1,30 @@
 # Author Niki
 import requests, json
+HOST = 'https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/'
+
 
 def set_lock(lock_var):
     if lock_var == 1 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/doors/lock', cert='pixelcamp.pem')
+        r = requests.put(HOST + 'doors/lock', cert='pixelcamp.pem')
     if lock_var == 0 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/doors/unlock', cert='pixelcamp.pem')
-        
+        r = requests.put(HOST + 'doors/unlock', cert='pixelcamp.pem')
+
     return (r.status_code,get_car())
 
 def set_immobilizer(immobilizer_var):
     if immobilizer_var == 1 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/immobilizer/engage', cert='pixelcamp.pem')
+        r = requests.put(HOST + 'immobilizer/engage', cert='pixelcamp.pem')
     if immobilizer_var == 0 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/immobilizer/disengage', cert='pixelcamp.pem')
-        
+        r = requests.put(HOST + 'immobilizer/disengage', cert='pixelcamp.pem')
+
     return (r.status_code,get_car())
 
 def set_tracking(tracking_var):
     if tracking_var == 1 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/livetracking/activate', cert='pixelcamp.pem')
+        r = requests.put(HOST + 'livetracking/activate', cert='pixelcamp.pem')
     if tracking_var == 0 :
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/livetracking/deactivate', cert='pixelcamp.pem')
-        
+        r = requests.put(HOST + 'livetracking/deactivate', cert='pixelcamp.pem')
+
     return (r.status_code,get_car())
 
 def set_geoFence(fence_var):
@@ -30,23 +32,23 @@ def set_geoFence(fence_var):
         json_object = json.loads(fence_var)
     except:
         return "it returned"
-    
+
     headers = {
     'accept': 'application/json',
     'content-type': 'application/json',
     }
-    
-    fenceRequest = requests.get('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/geofences', headers=headers, cert='pixelcamp.pem')
+
+    fenceRequest = requests.get(HOST + 'geofences', headers=headers, cert='pixelcamp.pem')
     fence_object = json.loads(fenceRequest.content)[0]
     if not fence_object:
-        r = requests.post('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/geofences', headers=headers, data=json_object, cert='pixelcamp.pem')
+        r = requests.post(HOST + 'geofences', headers=headers, data=json_object, cert='pixelcamp.pem')
     else:
         #print(fence_object['id'])
-        r = requests.put('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/geofences/'+fence_object['id'], headers=headers, data=json_object, cert='pixelcamp.pem')
-        
-    
+        r = requests.put(HOST + 'geofences/'+fence_object['id'], headers=headers, data=json_object, cert='pixelcamp.pem')
+
+
     return
-    
+
 
 
 def set_geo_radius():
@@ -66,4 +68,3 @@ def get_car():
     r = requests.get('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015', params=params, cert='pixelcamp.pem')
     print(json.dumps(r.json(),sort_keys=True, indent=2))
     return r.json()
-
