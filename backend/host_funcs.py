539162bdcf5213600408e29e9341192fed7ce832
Niki Berman <nika.berman@gmail.com>
2017-10-03 19:02:46 +0300

    comments added

--- backend/host_funcs.py
+++ backend/host_funcs.py
@@ -1,13 +1,15 @@
 # Author Niki
 
 from flask import Flask, request, jsonify
-from models_app import *
+from models_app import set_lock, set_immobilizer, set_tracking, set_geoFence, get_car
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
+
 
 @app.route('/api/set_lock', methods=['POST'])
 def set_lockAPI():
@@ -15,17 +17,20 @@ def set_lockAPI():
         return "Error", 400
     return jsonify(set_lock(request.json['lock'])), 200
 
+
 @app.route('/api/set_immobilizer', methods=['POST'])
 def set_immobilizerAPI():
     if not request.json or not 'immobilizer' in request.json:
         return "Error", 400
     return jsonify(set_immobilizer(request.json['immobilizer'])), 200
 
+
 @app.route('/api/set_tracking', methods=['POST'])
 def set_trackingAPI():
     if not request.json or not 'tracking' in request.json:
         return "Error", 400
     return jsonify(set_tracking(request.json['tracking'])), 200
+
 
 @app.route('/api/set_geoFence', methods=['POST'])
 def set_geoFenceAPI():
--- backend/models_app.py
+++ backend/models_app.py
@@ -4,6 +4,11 @@ HOST = 'https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015/'
 
 
 def set_lock(lock_var):
+    """
+    Lock/Unlock the car
+    :param lock_var: 1/0
+    :return: New car settings
+    """
     if lock_var == 1 :
         r = requests.put(HOST + 'doors/lock', cert='pixelcamp.pem')
     if lock_var == 0 :
@@ -11,23 +16,41 @@ def set_lock(lock_var):
 
     return (r.status_code,get_car())
 
+
 def set_immobilizer(immobilizer_var):
+    """
+    Activates/deactivates the immobilizer for the car
+    :param immobilizer_var: 1/0
+    :return: New car settings
+    """
     if immobilizer_var == 1 :
         r = requests.put(HOST + 'immobilizer/engage', cert='pixelcamp.pem')
     if immobilizer_var == 0 :
         r = requests.put(HOST + 'immobilizer/disengage', cert='pixelcamp.pem')
 
-    return (r.status_code,get_car())
+    return (r.status_code, get_car())
+
 
 def set_tracking(tracking_var):
+    """
+    Activates/deactivates live tracking for the car
+    :param tracking_var: 1/0
+    :return: New car settings
+    """
     if tracking_var == 1 :
         r = requests.put(HOST + 'livetracking/activate', cert='pixelcamp.pem')
     if tracking_var == 0 :
         r = requests.put(HOST + 'livetracking/deactivate', cert='pixelcamp.pem')
 
-    return (r.status_code,get_car())
+    return (r.status_code, get_car())
+
 
 def set_geoFence(fence_var):
+    """
+    Set car's accepted parameter
+    :param fence_var: polygon of geo points
+    :return: New car settings
+    """
     try:
         json_object = json.loads(fence_var)
     except:
@@ -46,23 +69,38 @@ def set_geoFence(fence_var):
         #print(fence_object['id'])
         r = requests.put(HOST + 'geofences/'+fence_object['id'], headers=headers, data=json_object, cert='pixelcamp.pem')
 
-
-    return
-
-
-
-def set_geo_radius():
-    pass
+    return (r.status_code, get_car())
 
 
 def set_speed():
+    """
+    Set a speed limit to the car
+    :return:
+    """
     pass
 
 
 def set_miles():
+    """
+    Set monthly mileage use
+    :return:
+    """
     pass
 
+
+def set_lock_time():
+    """
+    sets a timer for the car to be locked(immobilizer active)
+    :return:
+    """
+    pass
+
+
 def get_car():
+    """
+    The function returns all vihicle's data
+    :return: json file
+    """
     params = (
         ('fields', 'batteryLevel,connection.connected,connection.since,doors.allClosed,doors.leftOpen,doors.locked,doors.rightOpen,doors.trunkOpen,engineOn,fuelLevel,geo.latitude,geo.longitude,immobilizerEngaged,mileage,powerState,vin'),)
     r = requests.get('https://api.prod.smartservices.car2go.com/vega/vehicles/MOCKPIXEL015', params=params, cert='pixelcamp.pem')
